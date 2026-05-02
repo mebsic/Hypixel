b3b4f97c9035630d00c0b32bdb62e97b2a393f15
Andrei <39607018+mebsic@users.noreply.github.com>
2026-05-02 13:21:04 -0400

    Fix bugs

--- murdermystery/src/main/java/io/github/mebsic/murdermystery/manager/MurderMysteryGameManager.java
+++ murdermystery/src/main/java/io/github/mebsic/murdermystery/manager/MurderMysteryGameManager.java
@@ -94,7 +94,6 @@ public class MurderMysteryGameManager extends GameManager {
     private static final int DEFAULT_GOLD_PICKUP_TOKEN_REWARD = 10;
     private static final int DEFAULT_SURVIVE_30_SECONDS_TOKEN_REWARD = 50;
     private static final int DEFAULT_MURDERER_KILL_TOKEN_REWARD = 100;
-    private static final int HOTBAR_SLOT_ONE_INDEX = 0; // Slot 1 in the player's hotbar.
     private static final int KNIFE_HOTBAR_SLOT = 1; // Slot 2 in the player's hotbar.
     private static final int BOW_HOTBAR_SLOT = 1;
     private static final int ARROW_HOTBAR_SLOT = 2;
@@ -1058,7 +1057,6 @@ public class MurderMysteryGameManager extends GameManager {
             player.getInventory().setItem(ARROW_HOTBAR_SLOT, new ItemStack(Material.ARROW, 1));
         }
         syncGoldHotbarItem(player, mmPlayer);
-        player.getInventory().setHeldItemSlot(BOW_HOTBAR_SLOT);
         player.sendMessage(ChatColor.GREEN + "You picked up the bow! "
                 + ChatColor.GOLD + "You are now the Detective!");
         broadcast(ChatColor.YELLOW + "A player has picked up the Bow!");
@@ -1086,7 +1084,6 @@ public class MurderMysteryGameManager extends GameManager {
         }
         syncGoldHotbarItem(player, mmPlayer);
         if (!fromGold) {
-            player.getInventory().setHeldItemSlot(BOW_HOTBAR_SLOT);
             player.sendMessage(ChatColor.GREEN + "You picked up the bow! "
                     + ChatColor.GOLD + "GOAL: Find and kill the murderer!");
             broadcast(ChatColor.YELLOW + "A player has picked up the Bow!");
@@ -1316,7 +1313,6 @@ public class MurderMysteryGameManager extends GameManager {
             murdererSwordUnlocked = true;
             giveMurdererSwordNow();
             giveDetectiveBowNow();
-            forceGamePlayersToHotbarSlotOne();
             broadcastMurdererSwordReceivedMessageToOthers();
             updateScoreboardAll();
             return;
@@ -1404,7 +1400,7 @@ public class MurderMysteryGameManager extends GameManager {
             }
             ItemStack knife = createMurdererKnife(coreApi, player);
             player.getInventory().setItem(KNIFE_HOTBAR_SLOT, knife);
-            player.getInventory().setHeldItemSlot(HOTBAR_SLOT_ONE_INDEX);
+            player.getInventory().setHeldItemSlot(KNIFE_HOTBAR_SLOT);
             mmPlayer.markMurdererWeaponGrantedNow();
             player.sendMessage(ChatColor.YELLOW + "You have received your sword!");
             getTitleService().send(
@@ -1568,7 +1564,7 @@ public class MurderMysteryGameManager extends GameManager {
                 CoreApi coreApi = getPlugin().getCoreApi();
                 ItemStack knife = createMurdererKnife(coreApi, player);
                 player.getInventory().setItem(KNIFE_HOTBAR_SLOT, knife);
-                player.getInventory().setHeldItemSlot(HOTBAR_SLOT_ONE_INDEX);
+                player.getInventory().setHeldItemSlot(KNIFE_HOTBAR_SLOT);
                 selected.markMurdererWeaponGrantedNow();
             }
             showRoleTitle(player, selected.getRole());
@@ -1576,16 +1572,6 @@ public class MurderMysteryGameManager extends GameManager {
             player.sendMessage(ChatColor.GREEN + "The previous Murderer left, you are now taking their position!");
         }
         refreshMurdererLastInnocentSpeed();
-    }
-
-    private void forceGamePlayersToHotbarSlotOne() {
-        for (MurderMysteryGamePlayer mmPlayer : getMmPlayers()) {
-            Player player = Bukkit.getPlayer(mmPlayer.getUuid());
-            if (player == null || !player.isOnline()) {
-                continue;
-            }
-            player.getInventory().setHeldItemSlot(HOTBAR_SLOT_ONE_INDEX);
-        }
     }
 
     private void sendTeamingWarning(Player player, MurderMysteryRole role) {
